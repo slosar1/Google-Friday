1d05722f12bcd6463433c488fa6fb15e3a2950fe
slosar1 <lslosar@sjnma.org>
2017-12-08 22:17:38 -0600

    Google-Friday

---
+++ Time.py
@@ -0,0 +1,22 @@
+state = input("enter what state you are currently in (USA ONLY):")
+
+
+states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado',
+         'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho',
+         'Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
+         'Maine' 'Maryland','Massachusetts','Michigan','Minnesota',
+         'Mississippi', 'Missouri','Montana','Nebraska','Nevada',
+         'New Hampshire','New Jersey','New Mexico','New York',
+         'North Carolina','North Dakota','Ohio',
+         'Oklahoma','Oregon','Pennsylvania','Rhode Island',
+         'South  Carolina','South Dakota','Tennessee','Texas','Utah',
+         'Vermont','Virginia','Washington','West Virginia',
+         'Wisconsin','Wyoming'
+]
+
+
+print(state)
+
+from datetime iport datetime
+def astimezone(self, tz)
+	ifself

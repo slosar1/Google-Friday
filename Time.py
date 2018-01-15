78a8f134f2f83365bdeb59e7df5718f819fc1b1a
slosar1 <lslosar@sjnma.org>
2018-01-15 14:06:39 -0600

    Google_Friday

--- Time.py
+++ Time.py
@@ -15,8 +15,36 @@ states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado',
 ]
 
 
-print(state)
 
-from datetime iport datetime
-def astimezone(self, tz)
-	ifself
+Eastern = ["Maine", "Florida", "New York", "Rhode Island", "Georgia", "Ohio", "Kentucky", "New Hampshire", "Michigan", "Illinois", "Pennsylvania", "Vermont", "Massachusetts", "Connecticut", "New Jersey", "Delaware", "Maryland", "Virginia", "North Carolina", "South Carolina"]
+Central = ["North Dekota", "South Dekota", "Minnesota", "Wisconsin", "Illinois", "Nebraska", "Kansas", "Iowa", "Missouri", "Tennessee", "Oklahoma", "Arkansas", "Mississippi", "Alabama", "Louisiana", "Texas"]
+Mountain = ["Montana", "Idaho", "Wyoming", "Utah", "Colorado", "Arizona", "New Mexico"]
+Pacific = ["Washington", "Oregon", "Calafornia", "Nevada"]
+Alaskan = ["Alaska"]
+Hawaiin = ["Hawaii"]
+
+import datetime
+
+zone = {
+	"Eastern" : -5,
+	"Central" : -6,
+	"Mountain" : -7,
+	"Pacific" : -8,
+	"Alaskan" : -9,
+	"Hawaiin" : -10
+}
+
+now = datetime.datetime.now()
+
+if state in Eastern:
+	print(now-datetime.timedelta(hours = -5))
+elif state in Central:
+	print(now-datetime.timedelta(hours = -6))
+elif state in Mountain:
+	print(now-datetime.timedelta(hours = -7))
+elif state in Pacific:
+	print(now-datetime.timedelta(hours = -8))
+elif state in Alaskan:
+	print(now-datetime.timedelta(hours = -9))
+elif state in Hawaiin:
+	print(now-datetime.timedelta(hours = -10))
